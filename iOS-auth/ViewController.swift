8bf5335d5bf5e97ec0536505b07643e86c5c9c0f
MandyJCho <chomandy96@gmail.com>
2017-12-16 17:51:25 -0600

    Initial Commit

---
+++ iOS-auth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-auth
+//
+//  Created by Mandy Cho on 12/16/17.
+//  Copyright © 2017 Canvas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
