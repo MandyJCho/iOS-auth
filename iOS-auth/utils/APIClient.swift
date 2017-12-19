1beaaea27b578a2f285ab5e2e2605b6e203256d2
MandyJCho <chomandy96@gmail.com>
2017-12-19 01:30:53 -0600

    create foundation for HTTP requests

---
+++ iOS-auth/utils/APIClient.swift
@@ -0,0 +1,13 @@
+//
+//  APIClient.swift
+//  iOS-auth
+//
+//  Created by Mandy Cho on 12/19/17.
+//  Copyright © 2017 Canvas. All rights reserved.
+//
+
+import UIKit
+
+class APIClient: NSObject {
+
+}
---
+++ iOS-auth/utils/Helper.swift
@@ -0,0 +1,14 @@
+//
+//  Helper.swift
+//  iOS-auth
+//
+//  Created by Mandy Cho on 12/19/17.
+//  Copyright © 2017 Canvas. All rights reserved.
+//
+
+import Foundation
+
+
+func findENVVAR(key: String) {
+    guard let path = Bundle.main().pathForResource("env)
+}
